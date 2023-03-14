cb3403c9ab0124eec57ca7d2873c19526a489084
Nicolas Boisvert <nicklay@me.com>
2023-03-14 16:37:00 -0400

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -9,7 +9,14 @@ defmodule Loupe.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+          test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 

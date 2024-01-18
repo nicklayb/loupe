a5e24f80ba07a665c4fa5b67dac46216bbc9c9fc
Nicolas Boisvert <nicklay@me.com>
2024-01-18 13:57:03 -0500

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Loupe.MixProject do
   def project do
     [
       app: :loupe,
-      version: "0.6.0",
+      version: "0.6.1",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,

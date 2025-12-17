a5caa222518f26b88d81182a2b49b91442bb9fde
nicklayb <nicklay@me.com>
2025-12-17 14:49:22 -0500

    Add local hex and mix

--- flake.nix
+++ flake.nix
@@ -3,26 +3,34 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
-        in
-        {
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [ 
-              elixir_1_14
-              erlang_25
-              docker
-            ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            elixir_1_14
+            erlang_25
+            docker
+          ];
 
-            shellHook = ''
+          shellHook = ''
+            export MIX_HOME=./.nix-mix
+            export HEX_HOME=./.nix-hex
             mix deps.get
-            '';
-          };
-        }
-      );
+
+          '';
+        };
+      }
+    );
 }
